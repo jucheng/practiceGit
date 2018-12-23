66e37c444cfc50a1fce8a1ce232232ceeced4fdd
jucheng <l>
2018-12-23 14:27:32 +0800

    Revert "再次修改 test.js的文件"
    This reverts commit a3ca941bba73be56a41687bcbb1b914fe7e88cf2.

--- test.js
+++ test.js
@@ -2,6 +2,7 @@
 * @Author: woke
 * @Date:   2018-12-23 11:35:03
 * @Last Modified by:   woke
-* @Last Modified time: 2018-12-23 13:57:12
+* @Last Modified time: 2018-12-23 11:43:19
 */
 let desc = '我是测试的语句';
+console.log('desc');
