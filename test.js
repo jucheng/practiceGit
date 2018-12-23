a3ca941bba73be56a41687bcbb1b914fe7e88cf2
jucheng <l>
2018-12-23 13:57:49 +0800

    再次修改 test.js的文件

--- test.js
+++ test.js
@@ -2,7 +2,6 @@
 * @Author: woke
 * @Date:   2018-12-23 11:35:03
 * @Last Modified by:   woke
-* @Last Modified time: 2018-12-23 11:43:19
+* @Last Modified time: 2018-12-23 13:57:12
 */
 let desc = '我是测试的语句';
-console.log('desc');
